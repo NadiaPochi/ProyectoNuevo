65a8838ed1633f28a7baa836e8ad2639f9252ff1
Nadia <nadiaporciani@gmail.com>
2022-08-25 23:30:58 -0300

    Armando el backend1

---
+++ BackEnd/src/main/java/com/portfolio/np/Security/jwt/JwtProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.np.Security.jwt;
+
+import com.portfolio.np.Security.Entity.UsuarioPrincipal;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    public String generateToken(Authentication authentication) {
+        UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
+        return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime()+expiration*1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+    
+    
+}
